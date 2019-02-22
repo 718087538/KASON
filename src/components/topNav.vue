68d1bad8c1433eda95a384edf86c7823ef86bc89
Kason <718087538@qq.com>
2019-02-22 14:46:49 +0800

    添加了logo

--- src/components/topNav.vue
+++ src/components/topNav.vue
@@ -1,9 +1,13 @@
 <template>
     <div>
       <div class="my-nav layui-nav">
+        <div class="logo-box">
+        <img src="/static/images/logo.png" alt="" class="logo">
         <ul class="" lay-filter="">
           <li class="layui-nav-item layui-this"><a href="">首页</a></li>
+          <li class="layui-nav-item"><a href="">简述</a></li>
         </ul>
+     </div>
         <ul>
           <button class="layui-btn layui-btn-radius layui-btn-normal">注册</button>
           <button class="layui-btn layui-btn-radius">登录</button>
@@ -27,9 +31,20 @@
 
 <style scoped>
 .my-nav{
-  padding: 0 100px;
+  min-width: 960px;
+  padding: 0 6%;
   display: flex;
   align-items: center;
   justify-content: space-between;
+}
+.logo{
+  width: 150px;
+  height: 46px;
+  margin-right: 4%;
+}
+.logo-box{
+  flex: 1;
+  display: flex;
+  align-items: center;
 }
 </style>
