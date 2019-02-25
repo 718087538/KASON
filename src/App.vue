ae13a62accf27bf934d50dc6762c205e1b61626f
Kason <718087538@qq.com>
2019-02-25 18:08:56 +0800

    加入图片，但是遍历是不显示

--- src/App.vue
+++ src/App.vue
@@ -2,19 +2,22 @@
   <div id="app">
   <topNav></topNav>
     <swiper></swiper>
+    <classify></classify>
   <router-view/>
   </div>
 </template>
 
 <script>
-  import topNav from "./components/topNav";
+  import topNav from "./components/topNav"
   import swiper from './components/Swiper'
+  import classify from "./components/classify"
+
 export default {
   name: 'App',
   components:{
     topNav,
-    swiper
-
+    swiper,
+    classify
   }
 }
 </script>
---
+++ src/components/classify.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class=imgBox>
+      <div class="imgList">
+          <div v-for="item in items">
+            <img class="imgg" :src="item.src" alt="">
+            <h5>{{item.title}}</h5>
+            <P>{{item.info}}</P>
+          </div>
+      </div>
+      <img :src="srcc" alt="">
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "classify",
+      data(){
+          return{
+            items:[
+              {"src":"../assets/images/Vue.png",title:"VUE题",info:"学会之后，天下无敌"},
+              {"src":"../assets/images/Vue.png",title:"VUE题",info:"学会之后，天下无敌"},
+              {"src":"../assets/images/Vue.png",title:"VUE题",info:"学会之后，天下无敌"},
+              {"src":"../assets/images/Vue.png",title:"VUE题",info:"学会之后，天下无敌"},
+              {"src":"../assets/images/Vue.png",title:"VUE题",info:"学会之后，天下无敌"},
+            ],
+            srcc:"../assets/images/Vue.png"
+          }
+      }
+    }
+</script>
+
+<style scoped>
+  .imgBox{
+    background-color: #eeeeee;
+    padding: 30px 0;
+  }
+
+.imgList{
+  display: flex;
+  width: 1060px;
+  justify-content: space-between;
+  margin: 0 auto;
+  height: ;
+  background-color: #eee;
+}
+.imgg{
+  width: 90px;
+  height: 90px;
+}
+</style>
